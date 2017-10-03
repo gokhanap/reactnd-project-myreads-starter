772a72fa55e25c809bcba000b10b9d6d4b258bf0
gokhanap <gapay84@gmail.com>
2017-10-03 21:37:20 +0300

    shelf changer option gets working

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import './App.css'
 
 class BookShelfChanger extends React.Component {
 
+
   handleChange = (event) => {
-    this.setState({shelf: event.target.value})
+    this.props.book.shelf = event.target.value
     this.props.onChangeShelf(event.target.value, this.props.book)
-    console.log(event.target.value)
-    console.log(this.props.book)
+    //console.log(event.target.value)
+    console.log(this.props.book.shelf)
   }
 
   render() {
 
     return (
       <div className="book-shelf-changer">
-        <select onChange={this.handleChange}>
+        <select value={this.props.book.shelf} onChange={this.handleChange}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
-          <option value="none">None</option>
         </select>
       </div>
     )
